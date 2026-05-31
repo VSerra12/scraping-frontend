1f8e8f321eba1cbac27ff0f95b79559523401389
Victoria <vicky.serra.24@gmail.com>
2026-05-31 01:18:17 -0300

    cambio de nombre

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import "./App.css";
 import { Footer } from "./components/Footer";
 
 
+
 export default function App() {
   const [view, setView] = useState("search");
   const { toasts, push } = useToast();
